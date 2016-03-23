52a8578c2cd1214505a0d0880da9c0b8d91ea77c
GUEST <guestGUEST@GUESTs-iMac.local>
2016-03-23 10:58:28 -0400

    Modified app.jsx using new data

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+// import Note from './Note.jsx';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+
+  	const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
+    return (
+		<div>
+			<ul>{notes.map(note => <li key={note.id}>{note.task}</li> )}</ul>
+		</div>
+	);
   }
 }
