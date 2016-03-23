22148ff110fef1c71e45456e90fca94ea3f5531c
diegodesouza <diegotrus7@gmail.com>
2016-03-23 14:34:01 -0400

    Add alt container.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,89 +1,36 @@
 // import uuid from 'node-uuid';
 import React from 'react';
-import Firebase from 'firebase';
 import Items from './Items.jsx';
 import ItemActions from '../actions/ItemActions';
 import ItemStore from '../stores/ItemStore';
+import AltContainer from 'alt-container';
+import Rebase from 're-base';
+
+const base = new Rebase.createClass('https://appshoppinglist.firebaseio.com/items/');
 
 export default class App extends React.Component {
-  constructor(props) {
-    // get the context of 'this'
-    super(props);
-    this.firebaseRef = new Firebase('https://appshoppinglist.firebaseio.com/items/');
-
-    // this.state = {
-      // items: [
-        // {
-          // id: uuid.v4(),
-          // body: "Learn React",
-        // },
-        // {
-          // id: uuid.v4(),
-          // body: "Learn it well",
-        // },
-        // {
-          // id: uuid.v4(),
-          // body: "Learn it now",
-        // }
-      // ]
-    // };
-    
-    // set 'this' so we have access to it in our storeChanged
-    this.state = ItemStore.getState();
-  }
-
-  componentDidMount() {
-    ItemStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    ItemStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    this.setState(state);
-  };
-
   render() {
-    const items = this.state.items;
-
     return (
      <div>
         <button onClick={this.addItem}>+</button>
-        <Items 
-          items={items} 
-          onEdit={this.editItem}
-          onDelete={this.deleteItem}
-        />
+        <AltContainer
+          stores={[ItemStore]}
+          inject={{
+            items: () => ItemStore.getState().items
+          }}
+        >
+          <Items 
+            onEdit={this.editItem}
+            onDelete={this.deleteItem}
+          />
+        </AltContainer>
       </div>
     ); 
   } 
 
-  // addItem = () => {
-    // this.setState({
-      // items: this.state.items.concat([{
-        // id: uuid.v4(),
-        // body: 'New Item'
-      // }])
-    // });
-  // };
-
   addItem() {
     ItemActions.create({body: "New Item" })
   }
-
-  // editItem = (id, itemBody) => {
-    // if(!itemBody.trim()) {
-      // return;
-    // }
-    // const items = this.state.items.map(item => {
-      // if (item.id === id && itemBody) {
-        // item.body = itemBody;
-      // }
-      // return item;
-    // });
-    // this.setState({items});
-  // };
 
   editItem(id, body) {
     if(!body.trim()) {
@@ -91,14 +38,6 @@ export default class App extends React.Component {
     }
     ItemActions.update({id, body});
   }
-  // deleteItem = (id, event) => {
-    // // Avoid bubbling to edit
-    // event.stopPropagation();
-
-    // this.setState({
-      // items: this.state.items.filter(item => item.id !== id)
-    // });
-  // };
   
   deleteItem(id, event) {
     event.stopPropagation();
