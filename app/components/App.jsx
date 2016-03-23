be0e522ae3d1e66dd8bdfb9185f05049b3b2b263
GUEST <guestGUEST@GUESTs-iMac.local>
2016-03-23 11:19:40 -0400

    Added constructor method for initial state of app component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,27 +3,38 @@ import uuid from 'node-uuid';
 // import Note from './Note.jsx';
 
 export default class App extends React.Component {
-  render() {
+  
+	constructor(props) {
+		super(props);
 
-  	const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+	}
 
-    return (
-		<div>
-			<ul>{notes.map(note => <li key={note.id}>{note.task}</li> )}</ul>
-		</div>
-	);
-  }
+	render() {
+
+		const notes = this.state.notes;
+
+		console.log(notes);	
+
+		return (
+			<div>
+				<ul>{notes.map(note => <li key={note.id}>{note.task}</li> )}</ul>
+			</div>
+		);
+	}
 }
