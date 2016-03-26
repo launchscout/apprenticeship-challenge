96d2226c3e11e2f47e51c3334be49346fc42ff26
GUEST <guestGUEST@GUESTs-iMac.local>
2016-03-26 15:36:40 -0400

    Added test for Name component onClick

--- app/tests/name.js
+++ app/tests/name.js
@@ -6,8 +6,6 @@ import addAssertions from 'extend-tape';
 import jsxEquals from 'tape-jsx-equals';
 import Name from '../components/Name.jsx'
 
-console.log('Hello JavaScript');
-
 const test = addAssertions(tape, {jsxEquals});
 
 test('----- React Component Tests: Name -----', (t) => {
@@ -17,13 +15,23 @@ test('----- React Component Tests: Name -----', (t) => {
 	const component = createComponent.shallow(<Name name="shopping" />);
 
 	// Test component props and content
-	t.equal(component.text, 'shopping', 'name prop of component should be rendered as Name text "shopping list"');
+	t.equal(component.text, 'shopping', 'name prop of component should be rendered as Name text "shopping"');
 
+	
 	// Test rendered output
+	//Initial Render
 	renderer.render(<Name name="shopping" />);
 	let actualElement = renderer.getRenderOutput();
 	let expectedElement = <div onClick={function noRefCheck() {}}><span>shopping</span></div>;
+
+	// Simulate Click
+	renderer.getRenderOutput().props.onClick();	
+	let clickedElement = renderer.getRenderOutput();
+	let clickedExpectedElement = <input  ref={function noRefCheck() {}}  autoFocus={true}  defaultValue="shopping"  onBlur={function noRefCheck() {}}  onKeyPress={function noRefCheck() {}}  type="text"/>;
+
+
 	t.jsxEquals(actualElement, expectedElement);
+	t.jsxEquals(clickedElement, clickedExpectedElement);
 
 
 	t.end();
