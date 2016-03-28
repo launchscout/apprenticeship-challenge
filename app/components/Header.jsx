fbbb5c272f1dc0f72b54ab3ed81383c40c25e0a3
Robert Tassy <Rtass001@fiu.edu>
2016-03-28 00:44:46 -0400

    created list that updates on page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Title from './Header/Title';
 
 export default class Header extends React.Component {
+
+
   handleChange(e) {
     const title = e.target.value;
     this.props.changeTitle(title);
