4d6f7ef557e6027dc7ba46094f45505d7e181df3
Robert Tassy <Rtass001@fiu.edu>
2016-03-30 18:42:10 -0400

    Setting up core components - relocated cart logic

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,64 @@
 import './main.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+
+
+var App = React.createClass({
+
+  render : function() {
+    var name = "Robert";
+      return (
+        <div className="catch-of-the-day">
+          <div className="menu">
+            <Header />
+          </div>
+            <Order/>
+            <Inventory/>
+        </div>
+      )
+  }
+});
+
+var Header = React.createClass({
+
+  render : function() {
+    return (
+      <p>Header!</p>
+    )
+  }
+});
+
+var Order = React.createClass({
+
+  render : function() {
+    return (
+      <p>Order!</p>
+    )
+  }
+});
+
+var Inventory = React.createClass({
+
+  render : function() {
+    return (
+      <p>Inventory!</p>
+    )
+  }
+});
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    var name = "Robert";
+      return (
+        <form className= "store-selector">
+        {/*creating the store*/}
+          <h2>Please Enter a Store {name}</h2>
+          <input type='text' ref='storeID' required/>
+          <input type='Submit'/>
+        </form>
+      )
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
