a7fc0f24919174d4883a0c5ff00bc0bf6c8149f0
Michael Jasinski <majasinski8@gmail.com>
2016-04-02 10:40:48 -0500

    Add new item button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: uuid.v4(),
+          selection: 'dog food'
+        },
+        {
+          id: uuid.v4(),
+          selection: 'apples'
+        },
+        {
+          id: uuid.v4(),
+          selection: 'work on pj'
+        }
+      ]
+    };
   }
+
+  render() {
+    const items = this.state.items;
+
+    return (
+      <div>
+        <button onClick={this.addItem}>Add Item</button>
+        <ul>{items.map(item =>
+          <li key={item.id}>{item.selection}</li>
+        )}</ul>
+      </div>
+    );
+  }
+
+  addItem = () => {
+    this.setState({
+      items: this.state.items.concat([{
+        id: uuid.v4(),
+        selection: "New Item"
+      }])
+    });
+  }
+
+
 }
