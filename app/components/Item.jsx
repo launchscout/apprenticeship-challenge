06187b2182830a6d7d74fff319a42b3c0ad634cb
Michael Jasinski <majasinski8@gmail.com>
2016-04-02 11:49:29 -0500

    Add edit feature

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -1,4 +1,61 @@
 import React from 'react';
 
   //component definition
-  export default ({name}) => <div>{name}</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    // Render the component differently based on state.
+    if(this.state.editing) {
+        return this.renderEdit();
+    }
+      return this.renderItem();
+  }
+
+  renderEdit = () => {
+    return <input type="text"
+      ref={
+        (e) => e ? e.selectionStart = this.props.name.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.name}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+   };
+
+   renderItem = () => {
+      // If the user clicks a normal note, trigger editing logic.
+      return <div onClick={this.edit}>{this.props.name}</div>;
+    };
+
+    edit = () => {
+      // Enter edit mode.
+      this.setState({
+        editing: true
+      });
+    };
+
+    checkEnter = (e) => {
+      // The user hit *enter*, let's finish up.
+      if(e.key === 'Enter') {
+        this.finishEdit(e);
+      }
+    };
+    finishEdit = (e) => {
+      const value = e.target.value;
+
+      if(this.props.onEdit) {
+        this.props.onEdit(value);
+
+        // Exit edit mode.
+        this.setState({
+          editing: false
+        });
+      }
+    };
+}
