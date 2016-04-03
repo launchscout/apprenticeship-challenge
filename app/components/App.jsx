7c637adc443584e5170e8411f857dd6a47e75bc7
Michael Jasinski <majasinski8@gmail.com>
2016-04-03 08:55:29 -0500

    Add AltCont to app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,39 +1,25 @@
-// import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
-// import Item from './Item.jsx';
 import Items from './Items.jsx';
-
-
 import ItemActions from '../actions/ItemActions';
 import ItemStore from '../stores/ItemStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ItemStore.getState();
-  }
-
-  componentDidMount() {
-    ItemStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    ItemStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    this.setState(state);
-  };
 
   render() {
-    const items = this.state.items;
 
     return (
       <div>
         <button className="add-item" onClick={this.addItem}>Add Item</button>
-        <Items items={items}
-          onEdit={this.editItem}
-          onDelete={this.deleteItem} />
+
+        <AltContainer
+          stores={[ItemStore]}
+          inject={{
+            items: () => ItemStore.getState().items
+          }}
+         >
+          <Items onEdit={this.editItem}onDelete={this.deleteItem} />
+      </AltContainer>
       </div>
     );
   }
