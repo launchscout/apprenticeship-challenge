3ee55ac2129e965ce7bd3babdcf114f7f5b96b5e
Michael Jasinski <majasinski8@gmail.com>
2016-04-03 08:35:43 -0500

    Add local storage

--- app/index.jsx
+++ app/index.jsx
@@ -4,4 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
+
+
 ReactDOM.render(<App />, document.getElementById('app'));
