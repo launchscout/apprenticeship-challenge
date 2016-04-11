eee4232e97341b99b88c081f691ed1fb3571279f
Cameron Gordon <gordoncm@mail.uc.edu>
2016-04-10 22:14:28 -0400

    Added files via upload

--- install.sql
+++ install.sql
@@ -1,24 +1,64 @@
 create database if not exists shopping_list; 
 
 use shopping_list;
-	
+-- phpMyAdmin SQL Dump
+-- version 4.4.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 11, 2016 at 03:58 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `shopping_list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
 
 CREATE TABLE IF NOT EXISTS `items` (
   `current_user` text NOT NULL,
   `item_name` text NOT NULL,
   `item_sku` text NOT NULL,
   `item_price` text NOT NULL,
-  `list_id` text NOT NULL
+  `list_id` text NOT NULL,
+  `thumbnail_url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `items`
+--
+
+
+--
+-- Table structure for table `lists`
+--
 
 CREATE TABLE IF NOT EXISTS `lists` (
   `current_user` text NOT NULL,
   `list_name` text NOT NULL,
-  `list_id` text NOT NULL
+  `list_id` text NOT NULL,
+  `thumbnail_url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lists`
+--
+-----
+
 --
 -- Table structure for table `products`
 --
@@ -39,14 +79,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'asdfasf@asdfasf.com', 'asdfasdf');
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -66,4 +99,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
