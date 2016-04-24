01b75185d86024e1691334933d1ca0a3f551ce04
diegodesouza <diegotrus7@gmail.com>
2016-04-23 22:39:06 -0400

    Add tests for item form validation function.

--- app/validators/validate.js
+++ app/validators/validate.js
@@ -1,16 +1,16 @@
 const validate = (values) => {
   const errors = {}
   const validateSKU = new RegExp('^[a-z0-9A-Z]{10,20}$')
-  const validatePrice = new RegExp('\d{1,10}(\.\d{1,2})?')
+  const validatePrice = /^(?=.*[1-9])\d{0,5}(\.\d{1,2})?$/
 
   if (!values.sku || values.sku.trim() === '') {
     errors.sku = 'Enter a SKU'
   } else if (validateSKU.test(values.sku)) {
-    errors.sku = 'Invalid a SKU'
+    errors.sku = 'Invalid SKU'
   } else if (values.sku.length < 8) {
     errors.sku = 'SKU is too short'
   } else if (values.sku.length > 18) {
-    errors.sku = "SKU too is too long"
+    errors.sku = "SKU is too long"
   }
   if (!values.text || values.text.trim() === '') {
     errors.text = 'Enter an item'
@@ -19,7 +19,7 @@ const validate = (values) => {
   }
   if (!values.price || values.price.trim() === '') {
     errors.price = 'Enter a price'
-  } else if (validatePrice.test(values.price)) {
+  } else if (!validatePrice.test(values.price)) {
     errors.price = 'Invalid price'
   }
 
