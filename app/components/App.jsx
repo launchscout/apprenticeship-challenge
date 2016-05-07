339185a69446fe64258989e044562016a256f98e
LHAWLEY <hawleyleeanne@gmail.com>
2016-05-07 16:04:34 -0500

    Add 'add' functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,60 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+
+import List from './List.jsx';
+
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [
+      {
+        item: 'milk',
+        sku: uuid.v4(),
+        price: 2.00
+      },
+      {
+        item: 'eggs',
+        sku: uuid.v4(),
+        price: 1.50
+
+      },
+       {
+        item: 'oj',
+        sku: uuid.v4(),
+        price: 3.00
+      }
+    ]
+  };
+}
+
+
+  render(){
+    const list = this.state.list;
+
+    return (
+      <div>
+        <button onClick={this.addItem}>+</button>
+        <ul>
+          {list.map(list =>
+            <li key={list.sku}>
+              <List item='thing' sku={list.sku} price={list.price} />
+            </li>
+          )}
+        </ul>
+      </div>
+    );
   }
+  addItem = () => {
+    this.setState({
+      list: this.state.list.concat([{
+        item: 'New item',
+        sku: uuid.v4(),
+        price: 0
+      }])
+    });
+  };
 }
---
+++ app/components/List.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default (props) => <div>{props.item}<br/> SKU: {props.sku}<br/> ${props.price}</div>;
+
+
+
+
