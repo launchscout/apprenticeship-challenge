3258935cc4adf5fae5c4113ba134ed85f834fba3
Josh Stricker <jcstricke@gmail.com>
2016-05-10 01:41:39 -0400

    Moved index.html and bundle.js to Public folder for Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('build'));
+app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
