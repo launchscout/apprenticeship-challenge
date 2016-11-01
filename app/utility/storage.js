1476a9e5366ec15dbceab734b89333eefa661425
Anthony Ng <newyork.anthonyng@gmail.com>
2016-11-01 15:29:24 -0400

    Update storage to save Shopping List title

--- app/utility/storage.js
+++ app/utility/storage.js
@@ -1,11 +1,20 @@
 const Storage = (() => {
-	const save = (items) => {
-		localStorage.setItem('shopping_list', JSON.stringify(items));
+	const SHOPPING_LIST = 'shopping_list';
+	const SHOPPING_LIST_NAME = 'shopping_list_name';
+
+	const save = ({ list, title }) => {
+		localStorage.setItem(SHOPPING_LIST, JSON.stringify(list));
+		localStorage.setItem(SHOPPING_LIST_NAME, JSON.stringify(title));
 	};
 
 	const get = () => {
-		const list = localStorage.getItem('shopping_list');
-		return JSON.parse(list);
+		const list = JSON.parse(localStorage.getItem(SHOPPING_LIST));
+		const title = JSON.parse(localStorage.getItem(SHOPPING_LIST_NAME));
+
+		return {
+			list,
+			title
+		};
 	};
 
 	return {
--- app/utility/storage.test.js
+++ app/utility/storage.test.js
@@ -1,6 +1,6 @@
 import Storage from './storage';
 
-// Create localStorage mock
+// create localStorage mock
 const localStorageMock = (() => {
 	return {
 		setItem: jest.fn(),
@@ -11,20 +11,27 @@ Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
 describe('Storage', () => {
 	it('should call localStorage.setItem() on save', () => {
-		const list = ['a', 'b', 'c'];
-		Storage.save(list);
+		const info = {
+			list: ['a', 'b', 'c'],
+			title: 'Shopping List'
+		};
+		Storage.save(info);
 
 		const calls = localStorageMock.setItem.mock.calls;
-		expect(calls.length).toEqual(1);
+		expect(calls.length).toEqual(2);
 		expect(calls[0][0]).toEqual('shopping_list');
-		expect(calls[0][1]).toEqual(JSON.stringify(list));
+		expect(calls[0][1]).toEqual(JSON.stringify(info.list));
+
+		expect(calls[1][0]).toEqual('shopping_list_name');
+		expect(calls[1][1]).toEqual(JSON.stringify(info.title));
 	});
 
 	it('should call localStorage.getItem() on get', () => {
 		Storage.get();
 
 		const calls = localStorageMock.getItem.mock.calls;
-		expect(calls.length).toEqual(1);
+		expect(calls.length).toEqual(2);
 		expect(calls[0][0]).toEqual('shopping_list');
+		expect(calls[1][0]).toEqual('shopping_list_name');
 	});
 });
